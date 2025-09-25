5a486158d07526b85b23f6f0d9af27bf50bbbeef
Gnanavel <49830744+Gnanavel76@users.noreply.github.com>
2025-09-25 07:04:27 +0530

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,15 +2,11 @@ import axios from 'axios';
 import express, { Router } from 'express';
 import serverless from "serverless-http";
 
-
 const api = express();
 const TRADETRON_URL = 'https://api.tradetron.tech/api';
 
 // Entry and Exit signal from TradingView
 const MY_STGY_AUTH_TOKEN = '6c074f02-0d74-447e-ba06-6a4859a01fad'
-
-// Entry signal from TradingView and Exit from TradeTron
-const STGY2_AUTH_TOKEN = 'c1140b16-ee9f-44b3-b1d5-8d3a36609158'
 
 const expectedFromAddress = "noreply@tradingview.com";
 
@@ -38,12 +34,12 @@ router.get('/health', (req, res) => {
     res.send("OK")
 });
 
-router.post('/webhook', (req, res) => {
+router.post('/webhook', async (req, res) => {
     console.log(req.body)
     let alertMessage = req.body.summary
     let fromAddress = req.body.fromAddress
 
-    if (alertMessage === "Sample html content") {
+    if (alertMessage === "Sample Summary") {
         return res.send("Ok")
     }
     if (fromAddress !== expectedFromAddress) {
@@ -54,7 +50,6 @@ router.post('/webhook', (req, res) => {
         console.log("Alert message is null")
         return
     }
-
     const match = alertMessage.match(/nifty_action=([^ ]+)/);
     let action = null
     if (match) {
@@ -75,60 +70,44 @@ router.post('/webhook', (req, res) => {
     }
 
     const requests = [
-        axios.post(TRADETRON_URL, {
-            "auth-token": MY_STGY_AUTH_TOKEN,
-            key: actionValues.key,
-            value: actionValues.value
-        }, {
+        axios.post(TRADETRON_URL, {}, {
+            params: {
+                "auth-token": MY_STGY_AUTH_TOKEN,
+                key: actionValues.key,
+                value: actionValues.value
+            },
             headers: {
                 "Content-Type": 'application/json'
             }
         })
     ];
 
-    if (stopLoss) {
-        requests.push(
-            axios.post(TRADETRON_URL, {
-                "auth-token": STGY2_AUTH_TOKEN,
-                key: actionValues.key,
-                value: actionValues.value,
-                key1: "stop_loss",
-                value1: parseFloat(stopLoss)
-            }, {
-                headers: {
-                    "Content-Type": 'application/json'
-                }
-            })
-        )
-    }
+    try {
+        const results = await Promise.allSettled(requests)
 
-    Promise.allSettled(requests)
-        .then((results) => {
-            const successes = [];
-            const failures = [];
+        const successes = [];
+        const failures = [];
 
-            results.forEach((result, index) => {
-                if (result.status === "fulfilled") {
-                    successes.push(result.value);
-                    console.log(`Request ${index + 1} succeeded:`, result.value.config.data);
-                } else {
-                    failures.push(result.reason);
-                    console.error(`Request ${index + 1} failed:`, result.reason);
-                }
-            });
-
-            if (successes.length > 0) {
-                console.log("Partial success: Some requests completed successfully.");
+        results.forEach((result, index) => {
+            if (result.status === "fulfilled") {
+                successes.push(result.value);
+                console.log(`Request ${index + 1} succeeded:`, result.value.config.data);
             } else {
-                console.log("All requests failed.");
+                failures.push(result.reason);
+                console.error(`Request ${index + 1} failed:`, result.reason);
             }
-            res.send("Ok")
-        })
-        .catch((error) => {
-            console.error("Unexpected error:", error);
-            res.send("Ok")
         });
 
+        if (successes.length > 0) {
+            console.log("Partial success: Some requests completed successfully.");
+        } else {
+            console.log("All requests failed.");
+        }
+        res.send("Ok")
+    } catch (error) {
+        console.error("Unexpected error:", error);
+        res.send("Ok")
+    }
 });
 
 api.use("/api/", router);
